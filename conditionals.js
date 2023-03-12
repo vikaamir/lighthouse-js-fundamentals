cba72932491b47dac7de233e24ff243a3174eda0
Victoria Amirov <vika.amir20@gmail.com>
2023-03-11 19:23:08 -0800

    add change

--- conditionals.js
+++ conditionals.js
@@ -41,6 +41,7 @@
 // }
 // const whichSchool  = function (age) { 
 //   // Your code in here ...
+
 //   if ( age < 13 ) {
 //     return "Elementary School";
 //   } else if( age >= 13 && age <= 18 ) {
@@ -49,3 +50,5 @@
 //     return "Lighthouse Labs";
 //   }
 // } 
+
+
