3f7bf881c7ae6caed1f6d8bbb00cc11abc7442f4
Victoria Amirov <vika.amir20@gmail.com>
2023-03-15 19:22:17 -0700

    more

--- odd.js
+++ odd.js
@@ -1,5 +1,25 @@
-const isOdd = function(num){
-  return num % 2 === 0;
-}
-console.log("3 is odd: " + isOdd(3));
-console.log("8 is odd: " + isOdd(8));
+// const isOdd = function(num){
+//   return num % 2 === 0;
+// }
+// console.log("3 is odd: " + isOdd(3));
+// console.log("8 is odd: " + isOdd(8));
+
+
+
+
+// const finalPosition = function (moves) {
+//   var position_x = 0;
+//   var position_y = 0;
+//   for(var direction of moves){
+//     if(direction == "north"){
+//       position_y += 1;
+//     } else if(direction == "west"){
+//       position_x -= 1;
+//     } else if(direction == "east"){
+//       position_x += 1;
+//     } else if(direction == "south"){
+//       position_y -= 1;
+//     }
+//   }
+// }
+
