60389294d21ba736e6ab97e970a91ebb73c9aa4d
Victoria Amirov <vika.amir20@gmail.com>
2023-03-15 19:56:11 -0700

    banana bread loop

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var index = 0
+while (index < ingredients.length){
+  console.log(ingredients[index])
+  index++ 
+};
+ 
+for(let i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+ 
+for(let i = ingredients.length - 1; i >= 0 ; i--){
+  console.log(ingredients[i]);
+}
+
