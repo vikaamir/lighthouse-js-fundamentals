8af46bbdb29be0537228ec085c47daa788399fe4
Victoria Amirov <vika.amir20@gmail.com>
2023-03-15 19:22:44 -0700

    more

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,18 @@
-const chorus = "Let's dance!";
-for (let repeat = 0; repeat < 10; repeat++) {
-  console.log(chorus);
-}
-console.log("Until the sun comes up!");
+// const chorus = "Let's dance!";
+// for (let repeat = 0; repeat < 10; repeat++) {
+//   console.log(chorus);
+// }
+// console.log("Until the sun comes up!");
+// const amounts = [61.00, 52.25, 112.99, 5.00]; 
+// let total = 0;
+// for (let i = 0; i < amounts.length; i++) {
+//   total += amounts[i];
+// }
+// console.log('Order total is: ', total);
+// function isThisWorking(input) {
+//   console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+//   return "Returning: I am returning this string!";
+// }
+
+// isThisWorking(3);
+
